e1be9ad9795e14a2aaf7066f0803a8689a5ede1b
ljyh223 <3439426154@qq.com>
2025-02-28 00:42:15 +0800

    update api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 1
-        versionName = "1.18"
+        versionName = "1.21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
