d5b41a0db6e056b5ce4763d7e0eae4bfb97ced08
ljyh223 <3439426154@qq.com>
2025-04-14 14:31:43 +0800

    v1.32

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 1
-        versionName = "1.31"
+        versionName = "1.32"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
