dc1e1356ecaf63f0093a407afa5864e25298d523
ljyh223 <3439426154@qq.com>
2025-09-14 21:37:36 +0800

    简单维护一下

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 1
-        versionName = "1.35"
+        versionName = "1.36"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
