f6d8d1973ee3432b8acf8f376058d42569005aa5
ljyh223 <3439426154@qq.com>
2025-09-20 22:11:14 +0800

    播放列表支持拖拽

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ pluginManagement {
         maven { url=uri("https://maven.aliyun.com/repository/central") }
         maven { url=uri("https://maven.aliyun.com/repository/gradle-plugin") }
         maven { url = uri("https://jitpack.io") }
+        gradlePluginPortal()
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
