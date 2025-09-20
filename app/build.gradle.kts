8060b58d2559a2eb0a40107be2b82a4a0099aab0
ljyh223 <3439426154@qq.com>
2025-09-20 22:05:50 +0800

    播放列表支持拖拽

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 1
-        versionName = "1.36"
+        versionName = "1.37"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -130,6 +130,7 @@ dependencies {
     implementation(kotlin("reflect"))
 
     implementation(libs.android.gpuimage)
+    implementation(libs.reorderable)
 
 
 
