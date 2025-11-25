f4d822e201c599effa337d90575a8d944a9d735e
ljyh223 <3439426154@qq.com>
2025-11-25 18:51:22 +0800

    update to v1.43

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 1
-        versionName = "1.42"
+        versionName = "1.43"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
