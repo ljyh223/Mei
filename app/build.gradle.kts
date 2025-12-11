c079184fb4d86caf16ff94ee0aee065a82277662
ljyh223 <3439426154@qq.com>
2025-12-11 21:05:59 +0800

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 1
-        versionName = "1.44"
+        versionName = "1.45"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
