3ebbad0bfde19e7fc94c50424ebc814e1f3c8a6d
ljyh223 <3439426154@qq.com>
2025-12-30 17:34:59 +0800

    调整了一下主页

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 1
-        versionName = "1.47.1"
+        versionName = "1.47.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
