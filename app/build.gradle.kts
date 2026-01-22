33a701b828e9b47915ea82ca48a525c7468be284
ljyh223 <3439426154@qq.com>
2026-01-22 22:36:17 +0800

    update beta version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 1
-        versionName = "1.47.6"
+        versionName = "1.47.7-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
