78fbca2ea4ebba54cb92c6a4c5003849bac53550
ljyh223 <3439426154@qq.com>
2026-01-27 22:07:58 +0800

    update version to 1.48.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,9 @@
 
 pluginManagement {
     repositories {
-        maven { url=uri("https://maven.aliyun.com/repository/public") }
-        maven { url=uri("https://maven.aliyun.com/repository/central") }
-        maven { url=uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        // maven { url=uri("https://maven.aliyun.com/repository/public") }
+        // maven { url=uri("https://maven.aliyun.com/repository/central") }
+        // maven { url=uri("https://maven.aliyun.com/repository/gradle-plugin") }
         maven { url = uri("https://jitpack.io") }
         google {
             content {
